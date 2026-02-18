0ddf791cc2bb71ba4e7d6dcfe6b254878ad65b2c
heekoung-you <heekoung.you@ibm.com>
2026-02-18 14:27:08 +0100

    richtext address error

--- apps/frontend/src/components/factory.ts
+++ apps/frontend/src/components/factory.ts
@@ -1,11 +1,15 @@
 import 'server-only'
-import { type ComponentFactory, DefaultComponentFactory, RichTextComponentDictionary } from '@remkoj/optimizely-cms-react/rsc'
+import { type ComponentFactory, DefaultComponentFactory, RichTextComponentDictionary, createHtmlComponent } from '@remkoj/optimizely-cms-react/rsc'
 import cmsComponents from './cms'
 
 // Create the server factory, to be reused throughout the application
 export const factory : ComponentFactory = new DefaultComponentFactory()
 factory.registerAll(RichTextComponentDictionary)
 factory.registerAll(cmsComponents)
+
+// Ensure common HTML elements used inside rich text are registered so static
+// rendering doesn't warn about missing renderers (e.g. <address> nodes).
+factory.register('RichText/address', createHtmlComponent('address'))
 
 /**
  * Get the cached version of the Component Factory to use, this ensure that the
